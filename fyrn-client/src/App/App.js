ae26bcda609bf9c10a4113e8e70eb927e53ee0bd
cqlzx <bupt11lzx@163.com>
2017-10-12 00:22:52 -0700

    add NewsPanel

--- fyrn-client/src/App/App.js
+++ fyrn-client/src/App/App.js
@@ -1,13 +1,17 @@
+import 'materialize-css/dist/css/materialize.min.css';
+
 import React from 'react';
-import 'App.css';
+import logo from './logo.png';
+import './App.css';
+import NewsPanel from '../NewsPanel/NewsPanel';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <img className='logo' src={logo.png} alt='LOGO' />
+                <img className='logo' src={logo} alt='LOGO' />
                 <div className='container'>
-
+                    <NewsPanel></NewsPanel>
                 </div>
             </div>
         );
