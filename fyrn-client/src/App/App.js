d7d1d9698c15eb23b5ec615776b89bedccc17ccd
cqlzx <bupt11lzx@163.com>
2017-10-11 23:51:32 -0700

    add app js and css

---
+++ fyrn-client/src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <img className='logo' src={logo.png} alt='LOGO' />
+                <div className='container'>
+
+                </div>
+            </div>
+        );
+    };
+}
+
+export default App;
