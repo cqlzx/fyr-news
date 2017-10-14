33055ea09a0743bb00d03a31c49fb8883ae81976
cqlzx <bupt11lzx@163.com>
2017-10-14 11:58:54 -0700

    add load more news and debounce scroll

--- fyrn-client/src/NewsPanel/NewsPanel.js
+++ fyrn-client/src/NewsPanel/NewsPanel.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import NewsCard from "../NewsCard/NewsCard";
-
+import _ from 'lodash';
 
 class NewsPanel extends React.Component {
     constructor() {
         super();
 
+        this.handleScroll = this.handleScroll.bind(this);
         this.state = {news: null};
     }
 
     componentDidMount() {
-        this.loadNews();
+        this.loadMoreNews();
+        this.loadMoreNews = _.debounce(this.loadMoreNews, 1000);
+        window.addEventListener('scroll', this.handleScroll);
     }
 
-    loadNews() {
+    handleScroll() {
+        const scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+        if (scrollY + window.innerHeight >= document.body.offsetHeight - 50) {
+            console.log('Load more news...');
+            this.loadMoreNews();
+        }
+    }
+
+    loadMoreNews() {
         let url = 'http://' + window.location.host + '/news';
         const request = new Request(url, { method: 'GET', cache: false});
 
