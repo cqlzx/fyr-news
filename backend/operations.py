5aabe9aa6dd670035427252404925fb0a74c92bd
cqlzx <bupt11lzx@163.com>
2017-10-24 23:47:43 -0700

    change repository structure

--- backend/operations.py
+++ backend/operations.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'utils'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 
 import mongodb_client # pylint: disable=import-error
 
