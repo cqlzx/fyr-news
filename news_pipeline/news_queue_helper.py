f19e7c11f3627614b3f3efb9bd05109a3e348591
cqlzx <bupt11lzx@163.com>
2017-10-25 17:29:11 -0700

    add queue helper to clear queue

---
+++ news_pipeline/news_queue_helper.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+
+import news_api_client
+from cloud_amqp_client import CloudAmqpClient
+
+
+FYR_NEWS_QUEUE_URL = 'amqp://mlcafzrx:i3YEi-GptkW4ntHLh0mTV_zzc9qs4hGU@donkey.rmq.cloudamqp.com/mlcafzrx'
+FYR_NEWS_QUEUE_NAME = 'fyr-news-api-news'
+
+def clearQueue(queue_url, queue_name):
+    cloud_amqp_client = CloudAmqpClient(queue_url, queue_name)
+
+    num_of_messages = 0
+
+    while True:
+        message = cloud_amqp_client.get_message()
+
+        if message is None:
+            print 'Queue helper cleared %d messages' % num_of_messages
+            return
+
+        num_of_messages += 1
+
+
+if __name__ == '__main__':
+    clearQueue(FYR_NEWS_QUEUE_URL, FYR_NEWS_QUEUE_NAME)
