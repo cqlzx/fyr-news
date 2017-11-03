c61b2aefb4ff704359cfda308139b9e961b3d7d9
cqlzx <bupt11lzx@163.com>
2017-11-03 13:55:07 -0700

    add config and user model

--- web/fyrn-server/app.js
+++ web/fyrn-server/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -5,9 +6,13 @@ const app = express();
 const index = require('./routes/index');
 const news = require('./routes/news');
 
+//TODO: remove after development
+app.use(cors());
+
 app.use('/static', express.static(path.join(__dirname, '../fyrn-client/build/static')));
 app.use('/', index);
 app.use('/news', news);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ web/fyrn-server/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongooese');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        index: { unique: true}
+    },
+    password: String,
+});
+
+UserSchema.methods.comparePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, callback);
+};
+
+UserSchema.pre('save', function saveHook(next) {
+    const user = this;
+
+    //Proceed only if the password is modified or the user is new
+    if (!user.isModified('password')) return next();
+
+    return bcrypt.genSalt((saltError, salt) => {
+        if (saltError) {
+            return next(saltError);
+        }
+
+        return bcrypt.hash(user.password, salt, (hashError, hash) => {
+            if (hashError) {
+                return next(hashError);
+            }
+
+            user.password = hash;
+
+            return next();
+        });
+    });
+});
+
+module.exports = mongoose.model('User', UserSchema);
